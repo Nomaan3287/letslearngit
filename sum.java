6cf7f706ed0446fa15e6c9da09a302477a9f0826
nomaan32 <nomaan.pansare@neosoftmail.com>
2021-07-15 17:56:19 +0530

    message changed in sum.java

--- sum.java
+++ sum.java
@@ -2,4 +2,4 @@ public class sum{
 	public static void main(string[] a){
 		int num1=10, num2=20, sum;
 		sum =num1+num2;
-		System.out.println(sum);
+		System.out.println("Sum of the numbers is"+sum);
